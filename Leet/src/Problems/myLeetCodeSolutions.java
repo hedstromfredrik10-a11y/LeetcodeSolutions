3da014e7758f3d29b179dabb4facae47dbdc7dca
hedstromfredrik10-a11y <hedstromfredrik10@gmail.com>
2026-01-21 16:40:31 +0100

    1720

--- Leet/src/Problems/myLeetCodeSolutions.java
+++ Leet/src/Problems/myLeetCodeSolutions.java
@@ -1,8 +1,13 @@
 package Problems;
 
+import java.util.stream.IntStream;
+
 public class myLeetCodeSolutions {
     public static void main(String[] args) {
 
     }
 
+    public static int[] decode_1720(int[] encoded, int first) {
+
+    }
 }
